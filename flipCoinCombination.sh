5533f2fab1ff0f21a942aef8c869def4bf63cdb4
kdjay517 <kdjay517@gmail.com>
2021-10-12 08:51:05 +0530

    added uc2 into file

---
+++ flipCoinCombination.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+tailCount=0;
+headCount=0;
+
+declare -A singlet_dictionary;
+declare -A singlet_percentage;
+
+for ((i=1; i<=10; i++))
+do
+	coin=$((RANDOM%2))
+	if [ $coin -eq 1 ]
+	then
+		singlet_dictionary[$i]="H"
+		((headCount++))
+	else
+		singlet_dictionary[$i]="T"
+		((tailCount++))
+	fi
+done
+
+echo "Heads percentage-" $(($headCount*10))
+echo "Tails percentage-" $(($tailCount*10))
+echo "singlet output is" ${singlet_dictionary[@]}
+
+
